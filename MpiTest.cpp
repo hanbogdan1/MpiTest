5d30766d9890942d079c9d9d9bf38732f46b9817
Ureniuc Bogdan <ureniucbogdan@gmail.com>
2018-01-16 14:00:45 +0200

    hello world

--- MpiTest.cpp
+++ MpiTest.cpp
@@ -3,9 +3,16 @@
 
 #include "stdafx.h"
 #include "mpi.h"
+#include "stdio.h"
+#include <stdlib.h>
+#include <iostream>
+using namespace std;
 
-int main()
+int main(int argc, char ** argv)
 {
+	MPI_Init(&argc, &argv);
+	cout << "hello world";
+	MPI_Finalize();
     return 0;
 }
 
